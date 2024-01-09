c9f1f725569dac97b93bfeb1a974fb0ff69f661a
prashantmali-arambh <153917007+prashantmali-arambh@users.noreply.github.com>
2024-01-09 09:46:49 +0000

    updated

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,9 +1,9 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)
 
-console.log(marvel_heros);
+// console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
 // const allHeros = marvel_heros.concat(dc_heros)
@@ -20,9 +20,9 @@ console.log(marvel_heros);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Prashant"))
+// console.log(Array.from("Prashant"))
+// console.log(Array.from({name: "Prashant"})) // interesting
 
 let score1 = 100
 let score2 = 200
