cc9927f293336e858e876ab1523f786593081424
prashantmali-arambh <153917007+prashantmali-arambh@users.noreply.github.com>
2024-01-10 07:39:43 +0000

    updated

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,51 @@
+// const user = {
+//   username: "prashant",
+//   price: 999,
+
+//   welcomeMessage: function () {
+//     console.log(`${this.username} , welcome to website`);
+//     console.log(this);
+//   },
+// };
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai() {
+//     let username = "prashant";
+//   console.log(this.username);
+// }
+
+// chai();
+
+// const chai = function () {
+//   let username = "hitesh";
+//   console.log(this.username);
+// };
+// chai();
+
+// const chai = () => {
+//   let username = "prashant";
+//   console.log(this);
+// };
+
+// chai();
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => ({ username: "prashant" });
+
+// console.log(addTwo(3, 4));
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
