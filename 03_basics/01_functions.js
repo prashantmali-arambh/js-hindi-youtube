a2978661c1898c34fa89cbf087206203a53c3ce6
prashantmali-arambh <153917007+prashantmali-arambh@users.noreply.github.com>
2024-01-10 06:02:12 +0000

    modified

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,11 @@
-function sayMyName() {
-  console.log("H");
-  console.log("I");
-  console.log("T");
-  console.log("E");
-  console.log("S");
-  console.log("H");
-}
+// function sayMyName() {
+//   console.log("H");
+//   console.log("I");
+//   console.log("T");
+//   console.log("E");
+//   console.log("S");
+//   console.log("H");
+// }
 
 // sayMyName()
 
@@ -14,30 +14,30 @@ function sayMyName() {
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2) {
-  // let result = number1 + number2
-  // return result
-  return number1 + number2;
-}
+// function addTwoNumbers(number1, number2) {
+// let result = number1 + number2
+// return result
+//   return number1 + number2;
+// }
 
-const result = addTwoNumbers(3, 5);
+// const result = addTwoNumbers(3, 5);
 
 // console.log("Result: ", result);
 
-function loginUserMessage(username = "sam") {
-  if (!username) {
-    console.log("PLease enter a username");
-    return;
-  }
-  return `${username} just logged in`;
-}
+// function loginUserMessage(username = "sam") {
+//   if (!username) {
+//     console.log("PLease enter a username");
+//     return;
+//   }
+//   return `${username} just logged in`;
+// }
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
 
-function calculateCartPrice(val1, val2, ...num1) {
-  return num1;
-}
+// function calculateCartPrice(val1, val2, ...num1) {
+//   return num1;
+// }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
@@ -52,7 +52,7 @@ function handleObject(anyobject) {
   );
 }
 
-// handleObject(user)
+// handleObject(user);
 handleObject({
   username: "sam",
   price: 399,
@@ -61,8 +61,8 @@ handleObject({
 const myNewArray = [200, 400, 100, 600];
 
 function returnSecondValue(getArray) {
-  return getArray[1];
+  return getArray[0];
 }
 
-// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
