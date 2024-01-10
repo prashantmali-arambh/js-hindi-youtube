a69ac005710b32cf7c22e214d31a0cd4ea070630
prashantmali-arambh <153917007+prashantmali-arambh@users.noreply.github.com>
2024-01-10 10:21:07 +0000

    m

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -37,5 +37,5 @@ const userEmail = [];
 
 // condition ? true : false;
 
-const iceTeaPrice = 100;
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+// const iceTeaPrice = 100;
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
